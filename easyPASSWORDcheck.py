bc19e827040457296423174396b2bdd383216a0e
y4nnl2v <49566963+sofunc@users.noreply.github.com>
2023-05-25 18:15:17 +0800

    优化中

--- easyPASSWORDcheck.py
+++ easyPASSWORDcheck.py
@@ -6,14 +6,18 @@ with open('url_list.txt') as f:
     url_list = f.read().splitlines()
 url_list = [url for url in url_list if not url.startswith('#')]
 
-username_list = ['admin', 'root', 'guest']
+with open('usernames.txt') as f:
+    username_list = f.read().splitlines()
+
 password_list = ['123456', 'password', '1234567']
 
-proxy_list = ['ip1:port', 'ip2:port']  # 代理IP池
+enum_urls = []
+
+proxy_list = ['ip1:port', 'ip2:port']
 
 
 def scan(url, username, password, proxy):
-    proxy = {'http': proxy, 'https': proxy}  # 设置代理
+    proxy = {'http': proxy, 'https': proxy}
     data = {'username': username, 'password': password}
 
     try:
@@ -21,15 +25,20 @@ def scan(url, username, password, proxy):
         if '登录成功' in r.text or '管理中心' in r.text:
             with open('result.txt', 'a+') as f:
                 f.write(f'[+] {url} login succeeded! Username: {username} Password: {password}\n')
+            enum_urls.append(url)
     except:
-        pass  # 超时等异常忽略不计
+        pass
 
 
 for url in url_list:
-    if '/admin/' in url or '/login/' in url:  # 优先扫描敏感目录
+    if '/admin/' in url or '/login/' in url:
         for username in username_list:
             for password in password_list:
-                proxy = proxy_list[i % len(proxy_list)]  # 循环使用代理IP
+                proxy = proxy_list[i % len(proxy_list)]
                 i += 1
                 t = threading.Thread(target=scan, args=(url, username, password, proxy))
                 t.start()
+
+with open('enum_urls.txt', 'w') as f:
+    for url in enum_urls:
+        f.write(f'{url}\n')
