df4fccadef99670c04468cef15f06d265417b896
y4nnl2v <49566963+sofunc@users.noreply.github.com>
2023-05-30 11:16:43 +0800

    1.0.0

---
+++ AndroidManifest.xml文件批量检查/appTruck.py
@@ -0,0 +1,17 @@
+import os
+import xml.etree.ElementTree as ET
+
+for filename in os.listdir('apks'):
+    if filename.endswith('.xml'):
+        try:
+            tree = ET.parse(os.path.join('apks', filename))
+            root = tree.getroot()
+            # 检查debuggable标记
+            if 'android:debuggable' in root.attrib and root.attrib['android:debuggable'] == 'true':
+                print(f'{filename} 存在 debuggable=true 安全风险!')
+            # 检查allowBackup标记
+            if 'android:allowBackup' in root.attrib and root.attrib['android:allowBackup'] == 'true':
+                print(f'{filename} 存在 allowBackup=true 安全风险!')
+            # 其他检查...
+        except ET.ParseError as err:
+            print(f'解析 {filename} 發生错误:{err}')
